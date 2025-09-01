97cbb0e73e062561f16bc41d5abe57e5d2f6527a
kelvyn2012 <kudufo@gmail.com>
2025-09-01 20:57:26 +0100

    created my project url router

--- e_commerce_API/urls.py
+++ e_commerce_API/urls.py
@@ -1,23 +1,26 @@
-"""
-URL configuration for e_commerce_API project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from catalog.views import ProductViewSet, CategoryViewSet
+from users.views import UserViewSet, RegisterView, login_view
+from rest_framework.authtoken.views import obtain_auth_token
+from django.http import JsonResponse
+
+router = DefaultRouter()
+router.register(r"users", UserViewSet, basename="user")
+router.register(r"categories", CategoryViewSet, basename="category")
+router.register(r"products", ProductViewSet, basename="product")
+
+
+def home(request):
+    return JsonResponse({"message": "Welcome to the E-commerce API 🚀"})
+
 
 urlpatterns = [
+    path("", home),
     path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    path("api/auth/token/", obtain_auth_token, name="api_token_auth"),
+    path("api/auth/register/", RegisterView.as_view(), name="register"),
+    path("api/auth/login/", login_view, name="login"),
 ]
