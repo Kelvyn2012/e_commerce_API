8b56df55ab8f2eeac9ebcbadb23541c4a9900b0c
kelvyn2012 <kudufo@gmail.com>
2025-09-01 21:09:30 +0100

    updated the serializer of both users and catalog

---
+++ users/serializers.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data.get("email"),
+            password=validated_data["password"],
+        )
+        Token.objects.create(user=user)  # Create token for the new user
+        return user
