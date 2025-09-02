34be474bd857cf712c6e50819440e4108e8d2e10
kelvyn2012 <kudufo@gmail.com>
2025-09-02 21:40:20 +0100

    cleaning up my user serializer, removing the User model i imported

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
