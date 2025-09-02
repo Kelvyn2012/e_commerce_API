da3356c72369f5fa32e556534dd6d375d9c4fb46
kelvyn2012 <kudufo@gmail.com>
2025-09-02 21:56:43 +0100

    updated my views, removed the User since i have a get_user_model already

--- users/views.py
+++ users/views.py
@@ -1,5 +1,3 @@
-# views.py
-from django.contrib.auth.models import User
 from rest_framework import generics, permissions, viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
