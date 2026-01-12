6ac2754b0c57237d152cf7aed04911cc6ddd5b77
kelvyn2012 <kudufo@gmail.com>
2026-01-13 00:24:53 +0100

    migrated my database to Neon datbase

--- e_commerce_API/settings.py
+++ e_commerce_API/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
